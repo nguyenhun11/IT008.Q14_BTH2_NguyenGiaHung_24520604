2bc03e38336b199b33d6348bb951679e0c52e4b5
Gia Hưng Nguyễn <giahungcantho11@gmail.com>
2025-10-09 08:50:51 +0700

    Bai2

---
+++ Bai02/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+
+namespace Bai02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int isContinue;
+            string oldDir = "";
+            string mainDir = "";
+            do
+            {
+                Console.Write("Nhap duong dan thu muc: " + mainDir);
+                string dir = Console.ReadLine();
+
+                // Nếu nhập không trống thì cập nhật mainDir
+                if (!string.IsNullOrWhiteSpace(dir))
+                {
+                    oldDir = mainDir;
+                    if (dir[dir.Length - 1] == '/')
+                    {
+                        mainDir += dir;
+                    }
+                    else
+                    {
+                        mainDir += dir + "/";
+                    }
+                }
+
+                if (Directory.Exists(mainDir))
+                {
+                    DirectoryInfo di = new DirectoryInfo(mainDir);
+                    DirectoryInfo[] subDirs = di.GetDirectories();
+                    FileInfo[] files = di.GetFiles();
+
+                    foreach (DirectoryInfo dirInfo in subDirs)
+                    {
+                        Console.WriteLine("{0}\t<DIR>\t{1}",
+                            dirInfo.LastWriteTime.ToString("dd/MM/yyyy  HH:mm"),
+                            dirInfo.Name);
+                    }
+
+                    foreach (FileInfo file in files)
+                    {
+                        Console.WriteLine("{0}\t{1}\t{2}",
+                            file.LastWriteTime.ToString("dd/MM/yyyy  HH:mm"),
+                            file.Length,
+                            file.Name);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Khong thay thu muc");
+                    mainDir = oldDir;
+                }
+
+                Console.WriteLine("Tiep tuc?\n" +
+                    "[1]: Yes\n[0]: No");
+                isContinue = int.Parse(Console.ReadLine());
+            }
+            while (isContinue > 0);
+        }
+    }
+}
